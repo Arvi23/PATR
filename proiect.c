ec15541cd1e2aa1c6e4dc43ccf7eda7ea9d2a704
Arvi23 <carpatirtg@yahoo.ro>
2023-12-14 18:33:09 +0200

    Comenzi de testare adaugate si schimbat alarme

--- proiect.c
+++ proiect.c
@@ -11,13 +11,14 @@ pthread_cond_t cond;
 // Global variables
 const int MAX_CARS = 5; // Threshold for maximum cars in the tunnel
 int cars_in_tunnel = 0;
-float co2_level = 0.0;
+float smoke_level = 0.0;
 float natural_gas_level = 0.0;
-int pause_flag = 0; // Flag to pause the car entering thread
-int alert_flag = 0; // Flag to alert the car entering thread
+int pause_flag = 0;  // Flag to pause the car entering thread
+int alert_flag = 0;  // Flag to alert the car entering thread
+int output_flag = 1; // Flag to output the current state of the tunnel
 // Mutexes for each variable
 pthread_mutex_t cars_mutex;
-pthread_mutex_t co2_mutex;
+pthread_mutex_t smoke_mutex;
 pthread_mutex_t natural_gas_mutex;
 pthread_mutex_t cars_exiting_mutex;
 char command;
@@ -59,7 +60,7 @@ void *handle_user_input(void *arg)
             // Reset logic
             alert_flag = 0;
             cars_in_tunnel = 0;
-            co2_level = 0.0;
+            smoke_level = 0.0;
             natural_gas_level = 0.0;
             printf("\nResetting all sensor values and state of tunnel \n");
             printf("Please press C to continue...\n");
@@ -75,6 +76,87 @@ void *handle_user_input(void *arg)
             reset_terminal_mode();
             exit(0);
         }
+        else if (command == 's')
+        {
+            printf("\nSmoke level: %.1f\n", smoke_level);
+        }
+        else if (command == 'g')
+        {
+            printf("\nNatural gas level: %.1f\n", natural_gas_level);
+        }
+        else if (command == 't')
+        {
+            printf("\nCars in tunnel: %d\n", cars_in_tunnel);
+        }
+        else if (command == 'a')
+        {
+            printf("\nAlert flag: %d\n", alert_flag);
+        }
+        else if (command == 'o')
+        {
+            if (output_flag == 0)
+            {
+                printf("\nTurning on output...\n");
+                output_flag = 1;
+            }
+            else
+            {
+                printf("\nTurning off output...\n");
+                output_flag = 0;
+            }
+        }
+        else if (command == 'e')
+        {
+            printf("\nCars in tunnel: %d\n", cars_in_tunnel);
+            printf("Smoke level: %.1f\n", smoke_level);
+            printf("Natural gas level: %.1f\n", natural_gas_level);
+            printf("Alert flag: %d\n", alert_flag);
+            printf("Pause flag: %d\n", pause_flag);
+            printf("Output flag: %d\n", output_flag);
+        }
+        else if (command == '1')
+        {
+            smoke_level = 100;
+            printf("Smoke level: %.1f\n", smoke_level);
+        }
+        else if (command == '2')
+        {
+            natural_gas_level = 100;
+            printf("Natural gas level: %.1f\n", natural_gas_level);
+        }
+        else if (command == '3')
+        {
+            cars_in_tunnel = 10;
+            printf("Cars in tunnel: %d\n", cars_in_tunnel);
+        }
+        else if (command == '4')
+        {
+            alert_flag = 1;
+            printf("Alert flag: %d\n", alert_flag);
+        }
+        else if (command == 'h')
+        {
+            printf("\nCommands:\n");
+            printf("p - Pause\n");
+            printf("r - Reset\n");
+            printf("c - Continue\n");
+            printf("q - Quit\n");
+            printf("s - Print smoke level\n");
+            printf("g - Print natural gas level\n");
+            printf("t - Print cars in tunnel\n");
+            printf("a - Print alert flag\n");
+            printf("h - Print commands\n");
+            printf("e - Output current state of tunnel\n");
+            printf("o - Toggle state of tunnel output\n");
+            printf("1 - Set smoke level to 100\n");
+            printf("2 - Set natural gas level to 100\n");
+            printf("3 - Set cars in tunnel to 10\n");
+            printf("4 - Set alert flag to 1 to begin testing\n");
+        }
+        else if (command == ' ')
+        {
+            printf("\n");
+        }
         else
         {
             printf("\nInvalid command. Please try again.\n");
@@ -85,23 +167,24 @@ void *handle_user_input(void *arg)
     return NULL;
 }
 
-void *measure_co2(void *arg)
+void *measure_smoke(void *arg)
 {
     while (1)
     {
-        pthread_mutex_lock(&co2_mutex);
+        pthread_mutex_lock(&smoke_mutex);
         if (pause_flag == 0 && alert_flag == 0)
         {
-            co2_level = (rand() % 1000) / 10.0; // Random CO2 level between 0.0 and 99.9
-            printf("Measured CO2 level: %.1f\n", co2_level);
+            smoke_level = (rand() % 1000) / 10.0; // Random CO2 level between 0.0 and 99.9
+            if (output_flag == 1)
+                printf("Measured smoke level: %.1f\n", smoke_level);
         }
 
-        if (co2_level > 99.0)
+        if (smoke_level > 95.0)
         {
             alert_flag = 1;
         }
 
-        pthread_mutex_unlock(&co2_mutex);
+        pthread_mutex_unlock(&smoke_mutex);
         sleep(1); // Measure every second
     }
     while (pause_flag)
@@ -116,13 +199,13 @@ void *measure_natural_gas(void *arg)
     while (1)
     {
         pthread_mutex_lock(&natural_gas_mutex);
-        if (pause_flag == 0 && alert_flag == 0)
+        if (pause_flag == 0 && alert_flag == 0 && output_flag == 1)
         {
             natural_gas_level = (rand() % 1000) / 10.0; // Random Natural Gas level between 0.0 and 99.9
             printf("Measured Natural Gas level: %.1f\n", natural_gas_level);
         }
 
-        if (natural_gas_level > 99.0)
+        if (natural_gas_level > 95.0)
         {
             alert_flag = 1;
         }
@@ -143,15 +226,33 @@ void *count_cars_entering(void *arg)
     {
         if (alert_flag)
         {
-            printf("High CO2/Natural Gas levels. Pausing entry \n");
-            printf("Evacuating cars...\n");
-            while (cars_in_tunnel)
+            if (smoke_level > 95.0)
             {
-                sleep(1);
+                printf("Smoke detected. Turning sprinkler system on and turning ventilation system to 100%%\n");
+                printf("Operator has been alerted...\n");
+                while (smoke_level > 0)
+                {
+                    printf("Smoke level: %.1f\n", smoke_level);
+                    smoke_level -= rand() % 30;
+                    usleep(500000);
+                }
+                cars_in_tunnel = 0;
+                printf("Sprinkler system turned off. Resuming normal activity\n");
             }
-            co2_level = 0.0;
-            natural_gas_level = 0.0;
-            printf("Cars evacuated. Resuming entry...\n");
+            if (natural_gas_level > 95.0)
+            {
+                printf("Natural gas detected. Turning ventilation system to 100%%...\n");
+                printf("Playing alarm sound...\n");
+                while (natural_gas_level > 0)
+                {
+                    printf("Natural gas level: %.1f\n", natural_gas_level);
+                    natural_gas_level -= rand() % 30;
+                    usleep(500000);
+                }
+                printf("Ventilation system adjusted to normal levels. Resuming normal activity\n");
+                cars_in_tunnel = 0;
+            }
+
             alert_flag = 0;
         }
 
@@ -162,7 +263,8 @@ void *count_cars_entering(void *arg)
             pthread_cond_wait(&cond, &cars_mutex); // Wait for signal to resume
         }
         cars_in_tunnel++;
-        printf("Car entered. Cars in tunnel: %d\n", cars_in_tunnel);
+        if (output_flag == 1)
+            printf("Car entered. Cars in tunnel: %d\n", cars_in_tunnel);
         pthread_mutex_unlock(&cars_mutex);
 
         sleep(rand() % 3); // Time delay between 0 and 2 seconds
@@ -180,14 +282,15 @@ void *count_cars_exiting(void *arg)
     while (1)
     {
         pthread_mutex_lock(&cars_exiting_mutex);
-        if (cars_in_tunnel > 0)
+        if (cars_in_tunnel > 0 && alert_flag == 0)
         {
             cars_in_tunnel--;
-            printf("Car exited. Cars in tunnel: %d\n", cars_in_tunnel);
+            if (output_flag == 1)
+                printf("Car exited. Cars in tunnel: %d\n", cars_in_tunnel);
             pthread_cond_signal(&cond); // Signal the car entering thread
         }
         pthread_mutex_unlock(&cars_exiting_mutex);
-        sleep(rand() % 6); // Time delay between 0 and 9 seconds
+        sleep(rand() % 3); // Time delay between 0 and 3 seconds
 
         while (pause_flag)
         {
@@ -203,14 +306,14 @@ int main()
 
     // Initialize mutexes and condition variable
     pthread_mutex_init(&cars_mutex, NULL);
-    pthread_mutex_init(&co2_mutex, NULL);
+    pthread_mutex_init(&smoke_mutex, NULL);
     pthread_mutex_init(&natural_gas_mutex, NULL);
     pthread_cond_init(&cond, NULL);
 
     // Create threads
     pthread_create(&thread1, NULL, count_cars_entering, NULL);
     pthread_create(&thread2, NULL, count_cars_exiting, NULL);
-    pthread_create(&co2_thread, NULL, measure_co2, NULL);
+    pthread_create(&co2_thread, NULL, measure_smoke, NULL);
     pthread_create(&natural_gas_thread, NULL, measure_natural_gas, NULL);
     pthread_create(&user_input_thread, NULL, handle_user_input, NULL); // Create user input thread
 
@@ -224,7 +327,7 @@ int main()
 
     // Destroy mutexes and condition variable
     pthread_mutex_destroy(&cars_mutex);
-    pthread_mutex_destroy(&co2_mutex);
+    pthread_mutex_destroy(&smoke_mutex);
     pthread_mutex_destroy(&natural_gas_mutex);
     pthread_cond_destroy(&cond);
 
